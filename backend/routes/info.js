6f9b1247661eccf4d2c1e081d47796e201d71732
Aniket Chavan <chavananiket1272@gmail.com>
2024-08-02 23:12:42 +0530

    Update server to use PORT environment variable and bind to 0.0.0.0

--- backend/routes/info.js
+++ backend/routes/info.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Info = require('../models/infoModel');
+const Info = require('../models/cricketCubModel');
 const multer = require('multer');
 const path = require('path');
 
