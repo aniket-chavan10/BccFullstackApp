2f4f93c7a89947b510fa11b93e9a2b3381b25405
Aniket Chavan <chavananiket1272@gmail.com>
2024-08-02 12:14:39 +0530

    Update server to use PORT environment variable and bind to 0.0.0.0

--- backend/models/infoModel.js
+++ backend/models/infoModel.js
@@ -1,50 +1,22 @@
-// models/CricketClub.js
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CricketClubSchema = new Schema({
-  clubName: {
-    type: String,
-    required: false,
-  },
-  teamImg: {
-    type: String,
-    required: false,
-  },
-  associationName: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  tagline: {
-    type: String,
-    required: false,
-  },
-  logo: {
-    type: String,
-    required: false,
-  },
-  email: {
-    type: String,
-    required: false,
-  },
-  contactNumber: {
-    type: String,
-    required: false,
-  },
+const infoSchema = new Schema({
+  clubName: String,
+  associationName: String,
+  description: String,
+  tagline: String,
+  email: String,
+  contactNumber: String,
+  teamImg: String,
+  logo: String,
+  instaUrl: String,
   socialLinks: {
-    type: Map,
-    of: String,
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    facebook: String,
+    instagram: String,
+    twitter: String,
+    youtube: String,
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("CricketClub", CricketClubSchema);
+module.exports = mongoose.model('Info', infoSchema);
